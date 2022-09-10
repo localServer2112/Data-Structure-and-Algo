c3dd14468f079569b144dd96bb42d36487736e7d
Fawale Timmy <62085959+localServer2112@users.noreply.github.com>
2022-09-10 03:19:50 +0100

    push in a singly linked list

--- linkedlist.js
+++ linkedlist.js
@@ -10,3 +10,41 @@
 **/
 
 // singly linked list
+class Node{
+    constructor(_val){
+        this.val = _val;
+        this.next = null;
+    }
+}
+
+// let first = new Node("Hi");
+// first.next = new Node("There");
+// first.next.next = new Node("YO");
+// this is a low level implementation of a singly linked list
+
+class SinglyLinkedList{
+    // constructor for the basic property of a linked list
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    // create instance methods of the SLL class
+    push(val){
+        // this inserts to the end of the linked list
+        let newNode = new Node(val);
+
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+            // this.length+=1;
+        } else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+let listt = new SinglyLinkedList();
